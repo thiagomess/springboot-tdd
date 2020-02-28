14cf04e95997dbd4f8b72b4287398e5a7a2b7e78
thiagomess <thiagogomes19@hotmail.com>
2020-02-28 00:47:09 -0300

    Adicionando token

--- src/main/java/com/wallet/entity/Wallet.java
+++ src/main/java/com/wallet/entity/Wallet.java
@@ -13,8 +13,8 @@ import lombok.Data;
 
 @Entity
 @Data
-public class Wallet implements Serializable{
 
+public class Wallet implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
--- src/main/java/com/wallet/security/config/SecurityConfiguration.java
+++ src/main/java/com/wallet/security/config/SecurityConfiguration.java
@@ -1,22 +1,65 @@
 package com.wallet.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.wallet.security.JwtAuthenticationEntryPoint;
+import com.wallet.security.JwtAuthenticationTokenFilter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity(prePostEnabled = true) //habilita segurança com roles especificas em metodos Especificos usando 
+//a anotação @PreAuthorize("hasAnyRole('ADMIN')")
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private JwtAuthenticationEntryPoint unauthorizedHandler;
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
-			.exceptionHandling().and().sessionManagement()
-			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and().authorizeRequests()
-			.antMatchers("**").permitAll()
-			.anyRequest().authenticated();
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Autowired
+	public void configureAuthentication(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+		authenticationManagerBuilder.userDetailsService(this.userDetailsService).passwordEncoder(passwordEncoder());
 	}
+
+	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public JwtAuthenticationTokenFilter authenticationTokenFilterBean() throws Exception {
+		return new JwtAuthenticationTokenFilter();
+	}
+	
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+    	http.csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler);
+    	http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    	http.authorizeRequests()
+    	.antMatchers("/auth/**", "/configuration/security", "/webjars/**", "/users/**")
+		.permitAll().anyRequest().authenticated();
+		http.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
+		http.headers().cacheControl();
+    }
 }
